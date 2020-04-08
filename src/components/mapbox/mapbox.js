261ebd1fc5980102e1b380b88aaf358ca3e370fe
anozhenko <a.nozhenko@andagar.ru>
2020-04-08 04:47:07 +0300

    Карта вынесена в отдельный компонент MapBox

---
+++ src/components/mapbox/mapbox.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import ENV from "../../appData/env";
+import MapBoxGL from "mapbox-gl";
+import './mapbox.scss';
+
+class MapBox extends Component {
+    constructor(props) {
+        super(props);
+
+        this.mapRef = React.createRef();
+    }
+
+    componentDidMount() {
+        this.map = new MapBoxGL.Map({
+            container: this.mapRef.current,
+            style: 'mapbox://styles/mapbox/streets-v9',
+            accessToken: ENV.MAPBOX_ACCESS_TOKEN
+        });
+    }
+
+    componentWillUnmount() {
+        this.map.remove();
+    }
+
+    render() {
+        return (
+            <div ref={this.mapRef} className="mapbox_container"/>
+        );
+    }
+}
+
+export default MapBox;
--- src/pages/map/map.js
+++ src/pages/map/map.js
@@ -1,37 +1,19 @@
 import React, {Component} from 'react';
 import PageList from "../../appData/pageList";
 import MainLayout from "../../layouts/main";
-import MapBoxGL from 'mapbox-gl';
-import ENV from '../../appData/env';
+import MapBox from "../../components/mapbox";
+
 import './map.scss';
 
 
 class MapPage extends Component {
-    constructor(props) {
-        super(props);
-
-        this.mapRef = React.createRef();
-    }
-
-    componentDidMount() {
-        this.map = new MapBoxGL.Map({
-            container: this.mapRef.current,
-            style: 'mapbox://styles/mapbox/streets-v9',
-            accessToken: ENV.MAPBOX_ACCESS_TOKEN
-        });
-    }
-
-    componentWillUnmount() {
-        this.map.remove();
-    }
-
     render() {
         let {onChangePage} = this.props;
         let pageId = PageList.map.id;
 
         return (
             <MainLayout onChangePage={onChangePage} className={pageId} pageId={pageId}>
-                <div ref={this.mapRef} className="mapbox_container" />
+                <MapBox/>
                 <div>Форма</div>
             </MainLayout>
         );
