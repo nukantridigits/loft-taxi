aa367db9423f6888d792a837f3327c2efc78aec3
anozhenko <a.nozhenko@andagar.ru>
2020-04-30 22:34:34 +0300

    homework-4:
    Саги авторизации и регистрации

--- src/modules/auth/sagas.js
+++ src/modules/auth/sagas.js
@@ -1,20 +1,36 @@
-import {authRequest, authSuccess, authFailure, regRequest} from "./actions";
-import {call, put, select} from 'redux-saga/effects';
+import {authSuccess, authFailure} from "./actions";
+import {call, put} from 'redux-saga/effects';
 import {request} from '../../helpers/loftTaxiApi';
 
 const AUTH = 'auth';
-const REGISTER = 'register';
+const REGISTER = 'register1';
 
-export function* handleAuth(action) {
+export function* handleAuthotization(action) {
     try {
-        const auth = yield call(request, AUTH, action.payload);
-        console.log(auth)
+        const response = yield call(request, AUTH, action.payload);
+        const {success, token, error} = response;
+
+        if (success) {
+            yield put(authSuccess(token));
+        } else if (error) {
+            yield put(authFailure(error));
+        }
     } catch (error) {
-        console.error(error);
+        yield put(authFailure(error.message));
     }
 }
 
-/*
 export function* handleRegistration(action) {
-    console.log(action);
-}*/
+    try {
+        const response = yield call(request, REGISTER, action.payload);
+        const {success, token, error} = response;
+
+        if (success) {
+            yield put(authSuccess(token));
+        } else if (error) {
+            yield put(authFailure(error));
+        }
+    } catch (error) {
+        yield put(authFailure(error.message));
+    }
+}
--- src/modules/rootSaga.js
+++ src/modules/rootSaga.js
@@ -1,10 +1,10 @@
 import {fork} from 'redux-saga/effects';
-import {handleAuth, handleRegistration} from "./auth/sagas";
+import {handleAuthotization, handleRegistration} from "./auth/sagas";
 import {authRequest, regRequest} from "./auth";
 import {takeLatest, takeEvery} from "redux-saga/effects";
 
 
 export function* rootSaga() {
-    yield takeLatest(authRequest, handleAuth);
-    // yield takeLatest(regRequest, handleRegistration);
+    yield takeLatest(authRequest, handleAuthotization);
+    yield takeLatest(regRequest, handleRegistration);
 }
