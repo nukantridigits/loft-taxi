97ce447534c9974a1960d25df5d88fe7d5283529
anozhenko <a.nozhenko@andagar.ru>
2020-05-04 00:48:27 +0300

    homework-5:
    Форма профиля...

--- src/components/Forms/profileForm/profileForm.js
+++ src/components/Forms/profileForm/profileForm.js
@@ -4,18 +4,21 @@ import Typography from "@material-ui/core/Typography";
 import FormControl from "@material-ui/core/FormControl";
 import InputLabel from "@material-ui/core/InputLabel";
 import Input from "@material-ui/core/Input";
+import InputAdornment from "@material-ui/core/InputAdornment";
 import Button from "@material-ui/core/Button";
 import Card from '@material-ui/core/Card';
 import {fetchCardRequest, setCardRequest, getIsLoading, getIsExist, getData} from '../../../modules/card';
 import {getToken} from "../../../modules/auth";
 import {connect} from 'react-redux';
+import {Form, Field} from 'react-final-form';
+import formatString from "format-string-by-pattern";
+import {IconButton} from "@material-ui/core";
+import Box from '@material-ui/core/Box';
+import ClearIcon from "./clearIcon";
 import './profileForm.scss';
 
-const ProfileForm = ({isExist, token, isLoading, fetchCardRequest, setCardRequest}) => {
-    const [cardNumber, setCardNumber] = useState('');
-    const [expiryDate, setExpiryDate] = useState('');
-    const [cardName, setCardName] = useState('');
-    const [cvc, setCvc] = useState('');
+const ProfileForm = ({isExist, card, token, isLoading, fetchCardRequest, setCardRequest}) => {
+    // const [clearCardNameShow, setClearCardNameShow] = useState(false);
 
     useEffect(() => {
         if (!isExist) {
@@ -23,104 +26,156 @@ const ProfileForm = ({isExist, token, isLoading, fetchCardRequest, setCardReques
         }
     }, []);
 
-    const onFormSubmitHandler = event => {
-        event.preventDefault();
+    const rootClass = "form_profile";
 
-        if (!!cardNumber && !!expiryDate && !!cardName && !!cvc && !!token) {
-            return setCardRequest({
-                cardNumber, expiryDate, cardName, cvc, token
-            });
-        }
-
-        return false;
+    const initialValues = {
+        cardNumber: card.cardNumber,
+        expiryDate: card.expiryDate,
+        cardName: card.cardName,
+        cvc: card.cvc
     };
 
-    const handleCardNumberChange = event => {
-        return setCardNumber(event.target.value);
+    const masks = {
+        cardNumber: "9999 9999 9999 9999",
+        cvc: "999"
     };
 
-    const handleExpiryDateChange = event => {
-        return setExpiryDate(event.target.value);
+    const formatCardNumber = value => {
+        const onlyNumbers = value.replace(/[^\d]/g, '');
+        return formatString(masks.cardNumber, onlyNumbers);
     };
 
-    const handleCardNameChange = event => {
-        return setCardName(event.target.value);
+    const formatCvc = value => {
+        const onlyNumbers = value.replace(/[^\d]/g, '');
+        return formatString(masks.cvc, onlyNumbers);
     };
 
-    const handleCvcChange = event => {
-        return setCvc(event.target.value);
+    const formatCardName = value => {
+        return value.replace(/[\d!@#$%^&*()_]/g, '');
     };
+
+    const onSubmit = values => {
+        return setCardRequest({...values});
+    };
+
+    const PaymentForm = () => (
+        <Form onSubmit={onSubmit}
+              initialValues={initialValues}
+              mutators={{
+                  clearCardNumber: (args, state, utils) => {
+                      utils.changeValue(state, 'cardNumber', () => undefined)
+                  },
+                  clearCardName: (args, state, utils) => {
+                      utils.changeValue(state, 'cardName', () => undefined)
+                  },
+              }}
+        >
+            {({form, handleSubmit}) => (
+                <form className={`${rootClass} form_wrapper`} onSubmit={handleSubmit}>
+                    <Grid container align="center">
+                        <Grid item xs={12} className={`${rootClass}_header`}>
+                            <Typography variant="h4" component="h1">
+                                Профиль
+                            </Typography>
+                            <Typography component="p">
+                                Способ оплаты
+                            </Typography>
+                        </Grid>
+                    </Grid>
+
+                    <Grid container className="cards_row" justify="center" spacing={4}>
+                        <Grid item sm={6}>
+                            <Card className="card">
+                                <Grid container direction="column" justify="space-around" className="card_col">
+                                    <Field name="cardNumber"
+                                           parse={formatCardNumber}
+                                           render={({input, meta}) => (
+                                               <FormControl fullWidth={true} className="form_control">
+                                                   <InputLabel className={`${rootClass}_label`} htmlFor="cardNumber">
+                                                       Номер карты:
+                                                   </InputLabel>
+                                                   <Input className={`${rootClass}_input`} id="cardNumber" {...input}
+                                                          endAdornment={input.value.length >= 1 &&
+                                                          <InputAdornment position="end">
+                                                              <ClearIcon onClick={form.mutators.clearCardNumber}/>
+                                                          </InputAdornment>
+                                                          } required/>
+                                                   {meta.touched && meta.error && <span>{meta.error}</span>}
+                                               </FormControl>
+                                           )}
+                                    />
+                                    <Field name="expiryDate"
+                                           render={({input, meta}) => (
+                                               <FormControl fullWidth={true} className="form_control">
+                                                   <InputLabel className={`${rootClass}_label`} htmlFor="expiryDate">
+                                                       Срок действия:
+                                                   </InputLabel>
+                                                   <Input className={`${rootClass}_input`} id="expiryDate" {...input}
+                                                          required/>
+                                                   {meta.touched && meta.error && <span>{meta.error}</span>}
+                                               </FormControl>
+                                           )}
+                                    />
+                                </Grid>
+                            </Card>
+                        </Grid>
+                        <Grid item sm={6}>
+                            <Card className="card">
+                                <Grid container direction="column" justify="space-around" className="card_col">
+                                    <Field name="cardName"
+                                           parse={formatCardName}
+                                           render={({input, meta}) => (
+                                               <FormControl fullWidth={true} className="form_control">
+                                                   <InputLabel className={`${rootClass}_label`} htmlFor="cardName">
+                                                       Имя владельца:
+                                                   </InputLabel>
+                                                   <Input className={`${rootClass}_input`} id="cardName" {...input}
+                                                          inputProps={{maxLength: "20"}}
+                                                          endAdornment={input.value.length >= 1 &&
+                                                          <InputAdornment position="end">
+                                                              <ClearIcon onClick={form.mutators.clearCardName}/>
+                                                          </InputAdornment>
+                                                          } required/>
+                                                   {meta.touched && meta.error && <span>{meta.error}</span>}
+                                               </FormControl>
+                                           )}
+                                    />
+                                    <Field name="cvc"
+                                           parse={formatCvc}
+                                           render={({input, meta}) => (
+                                               <FormControl fullWidth={true} className="form_control">
+                                                   <InputLabel className={`${rootClass}_label`} htmlFor="cvc">
+                                                       CVC:
+                                                   </InputLabel>
+                                                   <Input className={`${rootClass}_input`} id="cvc" {...input}
+                                                          required/>
+                                                   {meta.touched && meta.error && <span>{meta.error}</span>}
+                                               </FormControl>
+                                           )}
+                                    />
+                                </Grid>
+                            </Card>
+                        </Grid>
+                    </Grid>
+
+                    <Grid item xs={12} align="center" className="form_footer">
+                        <Button className="btn"
+                                disabled={isLoading}
+                                data-testid="form-submit-btn"
+                                size="large"
+                                type="submit"
+                                variant="contained"
+                                color="primary">
+                            Сохранить
+                        </Button>
+                    </Grid>
+                </form>
+            )}
+        </Form>
+    );
 
     return (
-        <form className="form_profile form_wrapper"
-              onSubmit={onFormSubmitHandler}>
-            <Grid container align="center">
-                <Grid item xs={12} className="form_profile_header ">
-                    <Typography variant="h4" component="h1">
-                        Профиль
-                    </Typography>
-                    <Typography component="p">
-                        Способ оплаты
-                    </Typography>
-                </Grid>
-
-                <Grid container className="cards_row" justify="center" spacing={4}>
-                    <Grid item sm={6}>
-                        <Card className="card">
-                            <Grid container direction="column" justify="space-around" className="card_col">
-                                <FormControl fullWidth={true} className="form_control">
-                                    <InputLabel htmlFor="cardNumber">
-                                        Номер карты
-                                    </InputLabel>
-                                    <Input id="cardNumber" data-testid="cardNumber-input"
-                                           onChange={handleCardNumberChange} required/>
-                                </FormControl>
-                                <FormControl fullWidth={true} className="form_control">
-                                    <InputLabel htmlFor="expiryDate">
-                                        Срок действия
-                                    </InputLabel>
-                                    <Input id="expiryDate" data-testid="expiryDate-input"
-                                           onChange={handleExpiryDateChange} required/>
-                                </FormControl>
-                            </Grid>
-                        </Card>
-                    </Grid>
-
-                    <Grid item sm={6}>
-                        <Card className="card">
-                            <Grid container direction="column" justify="space-around" className="card_col">
-                                <FormControl fullWidth={true} className="form_control">
-                                    <InputLabel htmlFor="cardNumber">
-                                        Имя владельца
-                                    </InputLabel>
-                                    <Input id="cardName" data-testid="cardName-input"
-                                           onChange={handleCardNameChange} required/>
-                                </FormControl>
-                                <FormControl fullWidth={true} className="form_control">
-                                    <InputLabel htmlFor="expiryDate">
-                                        CVC
-                                    </InputLabel>
-                                    <Input id="cvc" data-testid="cvc-input"
-                                           onChange={handleCvcChange} required/>
-                                </FormControl>
-                            </Grid>
-                        </Card>
-                    </Grid>
-                </Grid>
-
-                <Grid item xs={12} align="center" className="form_footer">
-                    <Button className="btn"
-                            disabled={isLoading}
-                            data-testid="form-submit-btn"
-                            size="large"
-                            type="submit"
-                            variant="contained"
-                            color="primary">
-                        Сохранить
-                    </Button>
-                </Grid>
-            </Grid>
-        </form>
+        <PaymentForm/>
     );
 };
 
