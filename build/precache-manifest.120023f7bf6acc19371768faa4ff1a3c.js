3f6013a6435405bd0d24c38ec3a5a4782dd1420b
anozhenko <a.nozhenko@andagar.ru>
2020-05-07 21:34:34 +0300

    develop build

---
+++ build/precache-manifest.120023f7bf6acc19371768faa4ff1a3c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb4955a0410a6fb7f2bfb8b2d9eadf8a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa7adac465cc9f38c33a",
+    "url": "/static/css/main.fe753e99.chunk.css"
+  },
+  {
+    "revision": "c55615b0b9462d364fbf",
+    "url": "/static/js/2.bb1f990e.chunk.js"
+  },
+  {
+    "revision": "dbc03abe3e3aebbc41546c057eded6b9",
+    "url": "/static/js/2.bb1f990e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa7adac465cc9f38c33a",
+    "url": "/static/js/main.57bc2e02.chunk.js"
+  },
+  {
+    "revision": "0e5907e6c04d0dcea706",
+    "url": "/static/js/runtime-main.2d6edfcb.js"
+  },
+  {
+    "revision": "471e69aa895034b149f3aa32e98eef9f",
+    "url": "/static/media/bg.471e69aa.jpg"
+  },
+  {
+    "revision": "3ed311ad3d3788ab7eafdb3ea107f9d0",
+    "url": "/static/media/logo_dark.3ed311ad.svg"
+  },
+  {
+    "revision": "c5c72fa918ccf79dfebe1c5f2134798a",
+    "url": "/static/media/logo_light.c5c72fa9.svg"
+  },
+  {
+    "revision": "16cdbb0cafc11714e8c4c33995f61f0f",
+    "url": "/static/media/mastercard.16cdbb0c.png"
+  }
+]);
